068da77d552fcb6d15f2d762935e4952c7bf2aeb
enrico viola <naus3a@gmail.com>
2016-10-24 16:33:54 +0200

    made it work for OSX too

--- include/NatNetLinux/FrameListener.h
+++ include/NatNetLinux/FrameListener.h
@@ -27,6 +27,11 @@
 #include <utility>
 #include <time.h>
 
+#ifdef __MACH__
+#include <mach/clock.h>
+#include <mach/mach.h>
+#endif
+
 /*!
  * \brief Thread to listen for MocapFrame data.
  * \author Philip G. Lee
@@ -203,7 +208,17 @@ private:
          if( !select(sd+1, &rfds, 0, 0, &timeout) )
             continue;
          
+#ifdef __MACH__
+          clock_serv_t cclock;
+          mach_timespec_t mts;
+          host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+          clock_get_time(cclock, &mts);
+          mach_port_deallocate(mach_task_self(), cclock);
+          ts.tv_sec = mts.tv_sec;
+          ts.tv_nsec = mts.tv_nsec;
+#else
          clock_gettime( CLOCK_REALTIME, &ts );
+#endif
          dataBytes = read( sd, nnp.rawPtr(), nnp.maxLength() );
          
          if( dataBytes > 0 && nnp.iMessage() == NatNetPacket::NAT_FRAMEOFDATA )
