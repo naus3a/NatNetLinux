8a12ea1cd7c548aaf9b1e25ea190a391d6275d58
crazyflie <tempest@newmagic.com>
2016-12-19 11:48:27 -0500

    Wolfgang's latency fix

--- include/NatNetLinux/NatNet.h
+++ include/NatNetLinux/NatNet.h
@@ -263,6 +263,18 @@ public:
    float qy;
    float qz;
    float qw;
+    
+    float getAxis(int aId){
+        if(aId==0){
+            return qx;
+        }else if(aId==1){
+            return qy;
+        }else if(aId==2){
+            return qz;
+        }else{
+            return 0;
+        }
+    }
    
    //! \brief Default constructor. Without parameters, returns the identity (no rotation).
    Quaternion4f( float qx=0.f, float qy=0.f, float qz=0.f, float qw=1.f ) :
@@ -972,6 +984,10 @@ public:
          }
       }
       
+      // TODO: add version check
+      int numForcePlates = 0;
+      memcpy(&numForcePlates,data,4); data += 4;
+
       // Get latency/timecode
       memcpy(&_latency,data,4); data += 4;
       
@@ -1055,4 +1071,3 @@ std::ostream& operator<<(std::ostream& s, MocapFrame const& frame)
 }
 
 #endif /*NATNET_H*/
-
